fe6712e70b8709250f2d030df10d959ce14cc0c7
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2022-07-22 19:44:40 -0300

    Adding recognizer
    Add a speech recognizer and set the microphone as the audio source to capture the information.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#Out main file
+#Out main file.
+
+import speech_recognition as sr
+
+# Create a recognizer
+r = sr.Recognizer()
+
+# Open the microphone
+with sr.Microphone() as source:
+    while True:
+        audio = r.listen(source) # Sets the microphone as the audio source
+
+        print(r.recognize_google(audio, language="pt-br"))
